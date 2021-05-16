7976fc7e84790846bdc2fb3263ad8ee930a3b2bc
Анастасия Крутицкая <anastasia@MacBook-Pro-Anastasia.local>
2021-05-16 08:34:58 +0300

    first commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,13 @@
+import orderByProps, { obj, sortOrder } from '../app';
+
+test('orderByProps', () => {
+  const result = orderByProps(obj, sortOrder);
+  expect(result).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ]);
+});
+
---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+export const obj = { level: 2, name: 'мечник', health: 10, attack: 80, defence: 40 };
+
+export const sortOrder = ['name', 'level'];
+
+export default function orderByProps(object, sortOrderArray) {
+  const arr = [];
+  for (const prop in object) {
+    if (object.hasOwnProperty(prop)) {
+      arr.push({ key: prop, value: object[prop] });
+    }
+  }
+  const filterFirstPart = arr.filter((item) => {
+    return sortOrderArray.includes(item.key);
+  });
+  const sortedFirstPart = [];
+  sortOrderArray.forEach((item) => {
+    sortedFirstPart.push(filterFirstPart.find((i) => i.key === item));
+  });
+  const secondPart = [];
+  arr.forEach((item) => {
+    if (sortOrderArray.includes(item.key)) {
+      return;
+    }
+    secondPart.push(item);
+  });
+
+  secondPart.sort((a, b) => {
+    if (a.key < b.key) { return -1; }
+    return 1;
+  });
+
+  const sortedArray = [...sortedFirstPart, ...secondPart];
+  return sortedArray;
+}
